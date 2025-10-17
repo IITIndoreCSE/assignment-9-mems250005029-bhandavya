5790610aa8364736f606b029d4bedccf36a7c043
mems250005029-bhandavya <mems250005029@iiti.ac.in>
2025-10-17 16:31:22 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
-  struct item;
+  struct item{
 int id;
 float cost;
+};
 int main (){
-  item items[6];
-for(i=0;i<6;i++){
-if(item[i].cost>50){
-cout<<"Item ID:"<<item[i].id<<",Cost:"<<item[i].cost<<endl;
+  item items[6]={
+  {11,50.0},
+  {12,48.6},
+  {13,62.8},
+  {14,65.0},
+};
+for(int i=0;i<6;i++){
+if(items [i].cost>50){
+cout<<"Item ID:"<<items [i].id<<",Cost:"<<items [i].cost<<endl;
 }
 }
 return 0;
