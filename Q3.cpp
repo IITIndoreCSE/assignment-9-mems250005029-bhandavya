7197b53db83e8b1953bf7747b9ecd2c286232b32
mems250005029-bhandavya <mems250005029@iiti.ac.in>
2025-10-17 17:23:51 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+struct point{
+float x;
+float y;
+};
+int main(){
+  point points[7];
+int count=0;
+for(int i=0;i<7;i++){
+cout<<"enter x and y points of "<<i+1<<":"<<endl;
+cin>>points[i].x>>points[i].y;
+}
+for(int i=0;i<7;i++){
+if(points[i].x>0 && points[i].y>0){
+count++;
+}
+}
+cout<<"number of points on first quadrent are "<<count <<endl;
+return 0;
+}
 
