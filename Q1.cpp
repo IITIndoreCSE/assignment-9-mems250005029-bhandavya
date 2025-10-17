c347232cd989e55d78b5a908caa9ebc0081c675f
mems250005029-bhandavya <mems250005029@iiti.ac.in>
2025-10-17 16:10:27 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+  struct item;
+int id;
+float cost;
+int main (){
+  item items[6];
+for(i=0;i<6;i++){
+if(item[i].cost>50){
+cout<<"Item ID:"<<item[i].id<<",Cost:"<<item[i].cost<<endl;
+}
+}
+return 0;
+}
